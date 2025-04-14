31e728b9a64337ef72cce91739082fdb9c09ca22
andrew borzillo <dzillo1@gmail.com>
2025-04-14 07:58:40 -0400

    edits

--- VUE-comp/cart-item.php
+++ VUE-comp/cart-item.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order Options</title>
+    <title>cart item</title>
     <link rel="stylesheet" type="text/css" href="https://www.nutrisystem.com/css/website/bootstrap5/bootstrap-5.css" />
     <link rel="stylesheet" href="https://use.typekit.net/foj5idg.css">
 
