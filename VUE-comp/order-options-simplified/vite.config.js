1b3c71dd21c031429a39ef37ecc668394fd89a05
andrew borzillo <dzillo1@gmail.com>
2025-08-01 09:43:30 -0400

    edits

--- VUE-comp/order-options-simplified/vite.config.js
+++ VUE-comp/order-options-simplified/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/nscode/VUE-comp/order-options-simplified/',
+  base: '/mycode/ns-code/VUE-comp/order-options-simplified/',
 })
