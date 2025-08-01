2f414371ba4b8b91986b9e75730693c235a3d695
andrew borzillo <dzillo1@gmail.com>
2025-08-01 15:36:28 -0400

    edits

--- VUE-comp/order-options-simplified/vite.config.js
+++ VUE-comp/order-options-simplified/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path' // Import `resolve` from `path`
 
 export default defineConfig({
   plugins: [vue()],
   base: '/ns-code/VUE-comp/order-options-simplified/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html') // Add your index.html here
+      }
+    }
+  }
 })
