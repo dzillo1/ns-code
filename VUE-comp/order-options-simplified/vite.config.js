8d820e266a08e8fba66936fdcf388cb9c6cb0a82
andrew borzillo <dzillo1@gmail.com>
2025-08-01 15:40:41 -0400

    edits

--- VUE-comp/order-options-simplified/vite.config.js
+++ VUE-comp/order-options-simplified/vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path' // Import `resolve` from `path`
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/ns-code/VUE-comp/order-options-simplified/',
+  base: '/VUE-comp/order-options-simplified/',
   build: {
     rollupOptions: {
       input: {
