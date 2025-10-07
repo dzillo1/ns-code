83bfd4a767c7b6f1a3e5d1e1c82e061b50da1adb
andrew borzillo <dzillo1@gmail.com>
2025-10-07 11:41:21 -0400

    Trigger deploy workflow for order-options-simplified

--- js/coupon-code-scroll-to-form.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    if (window.location.hash === '#promo-offer') {   
-        $('.next-order a:first-of-type').click();
-        $('#couponNumber').val($('#couponNumber').val() + 'g6m56hvp');
-        $('html,body').animate({
-            scrollTop: $("#promoForm").offset().top
-        }, 5000);
-    }
-});
--- js/update-class-visibility-on-input-check.js
+++
@@ -1,8 +0,0 @@
-$( "#label1 input, #label2 input, #label3 input" ).change(function() {
-    var $label = $(this).closest('label');
-    var $addedBtn = $label.parent().find('.added-btn');
-    var checked = $(this).is(':checked');
-    
-    $label.toggleClass('add-cart', !checked);
-    $addedBtn.toggle(checked);
-});
