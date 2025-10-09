30221d0001446d0827c1e0b01b49e18143c77901
andrew borzillo <dzillo1@gmail.com>
2025-10-09 10:49:16 -0400

    Add order-options-simplified from restore-vuecomp-8d65b69

---
+++ VUE-comp/order-options-simplified/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import OrderOptions from './components/OrderOptions.vue'
+</script>
+
+<template>
+  <OrderOptions />
+</template>
+
+<style scoped>
+@import url("https://use.typekit.net/foj5idg.css");
+</style>
